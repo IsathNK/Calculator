66c341d94c8bf5f70f817266bd7977532d7c35bb
Isath Nenuka <gvisath@gmail.com>
2022-04-12 19:47:20 +0530

    Added New Feature
    Now It Creates A Log Of Your Results

--- calc.py
+++ calc.py
@@ -1,32 +1,54 @@
-#Dictionary
-op_convert = {
-    "+" : "add",
-    "-" : "sub",
-    "/" : "div",
-    "*" : "multi",
-}
+x=0
+while x==0:
+    resulttxt =open("results.txt" ,"a")
 
-#Input Variables
-firstNum = int(input("Enter First Number:"))
-operator = input("Enter The Operator:")
-secondNum = int(input("Enter Second Number:"))
+    #dictionary
+    op_convert = {
+        "+" : "add",
+        "-" : "sub",
+        "/" : "div",
+        "*" : "multi",
+        "^" : "sqr"
+    }
 
-#Functions
-def add():
-    result = firstNum + secondNum
-    print(result)
-def sub():
-    result = firstNum - secondNum
-    print(result)
-def multi():
-    result = firstNum * secondNum
-    print(result)
-def div():
-    result = firstNum / secondNum
-    print(result)
+    #Input Variables
+    firstNum = int(input("Enter First Number:"))
+    operator = input("Enter The Operator:")
+    secondNum = int(input("Enter Second Number:"))
+    global result 
 
-#If Entered A Opertor
-if operator == "+" or "-" or "/" or "*":
-    op = op_convert[operator]
-    eval(op+"()")
+    #Functions
+    def add():
+        global result 
+        result= firstNum + secondNum
+        print(result)
+    def sub():
+        global result 
+        result = firstNum - secondNum
+        print(result)
+    def multi():
+        global result 
+        result = firstNum * secondNum
+        print(result)
+    def div():
+        global result 
+        result = firstNum / secondNum
+        print(result)
+    def sqr():
+        global result 
+        result = firstNum**secondNum
+        print(result)
 
+        
+    #If Entered A Opertor
+    if operator == "+" or "-" or "/" or "*":
+        op = op_convert[operator]
+        eval(op+"()")
+    else:
+        print("Invalid Values")
+
+    #Write Results To A File
+    resulttxt =open("results.txt" ,"a+")
+    str(result)
+    resulttxt.write("\n" + str(result))
+    resulttxt.close()
